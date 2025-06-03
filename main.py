ddb10d3f829b3feffebb0428ed5f0c7cb59f7a5e
Ilias Benlarbi <Ilias.Benlarbi@gmail.com>
2025-06-03 15:16:32 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from neo4j import GraphDatabase
+import os
+
+app = FastAPI()
+
+
+URI = "bolt://localhost:7687"
+AUTH = ("neo4j", "123456789")
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+
+
+class EmployeeCreate(BaseModel):
+    name: str
+    emp_id: int
+
+
+
+@app.post("/employee")
+def create_employee(employee: EmployeeCreate):
+    with driver.session(database="test") as session:
+        # # Check if emp_id already exists
+        # result = session.run(
+        #     "MATCH (e:Employee {emp_id: $emp_id}) RETURN e",
+        #     emp_id=employee.emp_id
+        # )
+        # if result.single():
+        #     raise HTTPException(status_code=400, detail="Employee ID already exists")
+
+        # Create the Employee node
+        session.run(
+            "CREATE (e:Employee {name: $name, emp_id: $emp_id})",
+            name=employee.name,
+            emp_id=employee.emp_id
+        )
+
+    return {"message": "Employee node created", "employee": employee}
+
+
+
+@app.get("/employees")
+def get_all_employees():
+    with driver.session(database="test") as session:
+        result = session.run("MATCH (e:Employee) RETURN e.name AS name, e.emp_id AS emp_id")
+        employees = []
+        for record in result:
+            employees.append({
+                "name": record["name"],
+                "emp_id": record["emp_id"]
+            })
+    return employees
