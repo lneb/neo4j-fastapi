8052a1d424dcb998220481fcb6b31362bee2b92a
Ilias Benlarbi <Ilias.Benlarbi@gmail.com>
2025-06-03 19:30:58 +0200

    cicd test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI #, HTTPException
 from pydantic import BaseModel
 from neo4j import GraphDatabase, basic_auth
-import os
+
 
 app = FastAPI()
 
@@ -22,6 +22,7 @@ class EmployeeCreate(BaseModel):
     emp_id: int
 
 
+
 @app.post("/employee")
 def create_employee(employee: EmployeeCreate):
     with driver.session(database="test") as session:
@@ -37,25 +38,21 @@ def create_employee(employee: EmployeeCreate):
         session.run(
             "CREATE (e:Employee {name: $name, emp_id: $emp_id})",
             name=employee.name,
-            emp_id=employee.emp_id,
+            emp_id=employee.emp_id
         )
 
     return {"message": "Employee node created", "employee": employee}
 
 
+
 @app.get("/employees")
 def get_all_employees():
     with driver.session(database="test") as session:
-        result = session.run(
-            "MATCH (e:Employee) RETURN e.name AS name, e.emp_id AS emp_id"
-        )
+        result = session.run("MATCH (e:Employee) RETURN e.name AS name, e.emp_id AS emp_id")
         employees = []
         for record in result:
-            employees.append({"name": record["name"], "emp_id": record["emp_id"]})
+            employees.append({
+                "name": record["name"],
+                "emp_id": record["emp_id"]
+            })
     return employees
-
-
-# import uvicorn
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
